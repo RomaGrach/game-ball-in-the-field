71e7b4c7c694537c3c8e80d62c3fddb98c1ccfbc
stMaksim03 <91483289+stMaksim03@users.noreply.github.com>
2023-04-21 20:50:51 +0300

    Minor optimization
    21.04.2023 20:50

--- game ball in the field/BallInTheField/Assets/GOTY/Scripts/EndTrigger.cs
+++ game ball in the field/BallInTheField/Assets/GOTY/Scripts/EndTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trigger : MonoBehaviour
+public class StartTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -11,6 +11,6 @@ public class Trigger : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        FindAnyObjectByType<GameManager>().GetComponent<gameStart>()._Start();
+        if (other.gameObject.name == "Body") FindAnyObjectByType<GameManager>().GetComponent<gameStart>()._Start();
     }
 }
