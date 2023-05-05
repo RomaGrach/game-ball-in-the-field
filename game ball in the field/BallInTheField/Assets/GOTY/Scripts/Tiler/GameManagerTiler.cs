8f41f8e8b82da36010a550977c7c350f0dd905e4
stMaksim03 <91483289+stMaksim03@users.noreply.github.com>
2023-05-05 23:19:46 +0300

    New Level New problems
    05.05.2023 23:18

--- game ball in the field/BallInTheField/Assets/GOTY/Scripts/Tiler/GameManagerTiler.cs
+++ game ball in the field/BallInTheField/Assets/GOTY/Scripts/Tiler/GameManagerTiler.cs
@@ -23,7 +23,8 @@ public class GameManagerTiler : MonoBehaviour
     [SerializeField] Material DEFAULT;
     [SerializeField] int TileScore = 0;
     [SerializeField] float StartInterval = 5;
-    [SerializeField] float StartTime = 5;  
+    [SerializeField] float StartTime = 5;
+    [SerializeField] int CorrectCap = 2;
     public bool RandomisedIntervals = true;
     public float Interval = 0;
     public float T = 0;
@@ -31,11 +32,19 @@ public class GameManagerTiler : MonoBehaviour
     public float counter;
     private bool Flag = true;
     private bool Flag2 = true;
+    private bool Flag3 = true;
+    private bool RandFlag = true;
+    private char MainColor;
+    private string colorLane;
     [SerializeField] GameObject Tiles;
+    [SerializeField] GameObject Floor;
+    MeshRenderer[] TileArray;
+    BoxCollider[] TileBoxArray;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TileArray = Tiles.GetComponentsInChildren<MeshRenderer>();
+        TileBoxArray = Tiles.GetComponentsInChildren<BoxCollider>();
     }
 
     // Update is called once per frame
@@ -54,7 +63,11 @@ public class GameManagerTiler : MonoBehaviour
         }
         else
         {
-            if (ttime > 1.6f*StartTime && RandomisedIntervals && T == 0f) Interval = Random.Range(0.5f * StartInterval, 1.1f * StartInterval);
+            if (RandomisedIntervals && RandFlag)
+            {
+                Interval = Random.Range(0.5f * StartInterval, 1.1f * StartInterval);
+                RandFlag = false;
+            }
             else Interval = StartInterval;
             T += 0.02f;
             if (T >= Interval && Flag)
@@ -72,30 +85,51 @@ public class GameManagerTiler : MonoBehaviour
 
                 if (counter == 0) Flag = false;
             }
-            else if(!Flag && Flag2)
+            else if (!Flag && Flag2)
             {
-                string colorLane = RandomColorGen();
+                colorLane = RandomColorGen();
                 MeshRenderer[] TileArray = Tiles.GetComponentsInChildren<MeshRenderer>();
-                if (colorLane[0] == 'r') Screen.GetComponent<MeshRenderer>().material = RED;
-                if (colorLane[0] == 'b') Screen.GetComponent<MeshRenderer>().material = BLUE;
-                if (colorLane[0] == 'g') Screen.GetComponent<MeshRenderer>().material = GREEN;
+                if (MainColor == 'r') Screen.GetComponent<MeshRenderer>().material = RED;
+                if (MainColor == 'b') Screen.GetComponent<MeshRenderer>().material = BLUE;
+                if (MainColor == 'g') Screen.GetComponent<MeshRenderer>().material = GREEN;
+                int C = 0;
                 for (int i = 0; i < TileArray.Length; i++)
                 {
+                    if (colorLane[i] == MainColor) C++;
                     if (colorLane[i] == 'r') TileArray[i].material = RED;
                     if (colorLane[i] == 'b') TileArray[i].material = BLUE;
                     if (colorLane[i] == 'g') TileArray[i].material = GREEN;
-                }
+                }  
                 Flag2 = false;
                 oldT = T;
             }
-            else if(!Flag && !Flag2 && Mathf.Abs(T - oldT) >= 0.6f*Interval)
+            else if (!Flag && !Flag2 && Flag3 && Mathf.Abs(T - oldT) >= 0.6f * Interval)
             {
-                
-                MeshRenderer[] TileArray = Tiles.GetComponentsInChildren<MeshRenderer>();
+                for (int i = 0; i < TileBoxArray.Length; i++)
+                {
+                    if (colorLane[i] != MainColor) { 
+                        TileBoxArray[i].enabled = false;
+                        TileArray[i].enabled = false;
+                    }
+                }
+                Floor.SetActive(false);
+                oldT = T;
+                Flag3 = false;
+            }
+            else if (!Flag && !Flag2 && !Flag3 && Mathf.Abs(T - oldT) >= 0.4f * Interval)
+            {
+                for (int i = 0; i < TileBoxArray.Length; i++)
+                {
+                    TileBoxArray[i].enabled = true;
+                    TileArray[i].enabled = true;
+                }
+                Floor.SetActive(true);
                 for (int i = 0; i < TileArray.Length; i++) TileArray[i].material = DEFAULT;
                 T = 0f;
                 Flag = true;
                 Flag2 = true;
+                Flag3 = true;
+                RandFlag = true;
             }
 
 
@@ -104,11 +138,30 @@ public class GameManagerTiler : MonoBehaviour
     string RandomColorGen(int tileN = 9, string colors = "rbg")
     {
         string colorL = "";
-        int r = colors.Length;
+        int C = 0;
+        int r = 0;
+        bool f = true;
+        bool check = true;
+        MainColor = colors[Random.Range(0, colors.Length)];
         for (int i = 0; i < tileN; i++)
         {
-            colorL += colors[Random.Range(0,r)];
+            if (C >= CorrectCap && f)
+            {
+                colors = colors.Remove(colors[colors.IndexOf(MainColor)]);
+                f = false;
+                print(colors);
+            }
+            r = colors.Length;    
+            char c = colors[Random.Range(0, r)];
+            colorL += c;
+            if (c == MainColor)
+            {
+                C++;
+                check = false;
+            }
+            print(C);
         }
+        if (check) colorL.Replace(colors[Random.Range(0, r)],MainColor);
         return colorL;
     }
 }
